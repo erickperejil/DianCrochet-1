3c957c65e78cfa339113d1dd13e4f3a373fd144e
Erick Daniel Escoto Perez <erickdanielescoto@gmail.com>
2024-10-03 21:59:12 -0600

    Estructuracion de Registro

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,5 +1,5 @@
 import "../../globals.css";
-import LoginForm from "../components/posts/LoginForm";
+import RegisterForm from "../components/posts/RegisterForm";
 import LoginBG from "../components/backgrounds/LoginBackGround";
 
 export default function Login() {
@@ -7,7 +7,7 @@ export default function Login() {
     <main className="flex h-screen w-full flex-col bg-slate-50 overflow-hidden">
       <nav className="h-[8%] w-full bg-slate-200 z-20"></nav>
       <section className="imagen relative flex h-[92%] w-full items-center justify-center">
-        <LoginForm/>
+        <RegisterForm/>
         <LoginBG/>
       </section>
     </main>
