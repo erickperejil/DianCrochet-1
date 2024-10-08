cb7431ff07d3d70c39842f24176d4c0d8737125f
Alejandra Solano <alejandra133valle@gmail.com>
2024-10-08 02:52:37 -0600

    ventana modal y redireccionamiento

--- src/app/auth/components/posts/PswdCodeForm.tsx
+++ src/app/auth/components/posts/PswdCodeForm.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-export default function PswCodeForm() {
+interface PswdCodeFormProps {
+  email: string;
+}
+
+export default function PswdCodeForm({ email }: PswdCodeFormProps) {
   return (
     <form className="relative z-10 h-[60.4%] w-[25.7%] rounded-3xl bg-white opacity-90 shadow-2xl">
       <div className="absolute top-[5.3%] flex h-[12.5%] w-full items-center justify-center">
@@ -10,7 +14,7 @@ export default function PswCodeForm() {
       </div>
       <div className="absolute top-[23.3%] flex h-[10.6%] w-full ml-7">
         <h5 className="w-[70.1%] font-lekton text-l text-gray-600 font-normal">
-          Ingrese el codigo
+          Ingrese el código
         </h5>
       </div>
       <div className="absolute top-[35%] flex h-[7.6%] w-full justify-center mt-6">
@@ -22,14 +26,13 @@ export default function PswCodeForm() {
         />
       </div>
       <div className="absolute bottom-[35%] left-4 pl-3">
-        <h1 className="font-lekton text-sm text-[#535353] underline decoration-[#535353]">¿No recibiste ningun codigo?</h1>          
+        <h1 className="font-lekton text-sm text-[#535353] underline decoration-[#535353]">¿No recibiste ningún código?</h1>          
       </div>
       <div className="absolute bottom-[15%] flex h-[10.19%] w-full justify-center">
         <button className="absolute flex h-full w-[56.61%] items-center justify-center rounded-3xl bg-[#C68EFE] pt-[1%]">
           <h1 className="w-[88.1%] font-koulen text-2xl text-white">
             ENVIAR 
           </h1>
-          
         </button>
       </div>
       <div className="absolute bottom-4 left-4">
--- src/app/auth/fgt-pwd-code/page.tsx
+++ src/app/auth/fgt-pwd-code/page.tsx
@@ -1,15 +1,19 @@
+"use client";
+import { useSearchParams } from "next/navigation";
 import PswdCodeForm from "../components/posts/PswdCodeForm";
 import LoginBG from "../components/backgrounds/LoginBackGround";
 
-export default function PswCode(){
-    return(
+export default function PswCode() {
+  const searchParams = useSearchParams();
+  const email = searchParams.get("email");
+
+  return (
     <main className="flex h-screen w-full flex-col bg-slate-50 overflow-hidden">
       <nav className="h-[8%] w-full bg-slate-200 z-20"></nav>
       <section className="imagen relative flex h-[92%] w-full items-center justify-center">
-      <PswdCodeForm/>
-        <LoginBG/>
+        <PswdCodeForm email={email || ""} />
+        <LoginBG />
       </section>
     </main>
-    );
- 
+  );
 }
