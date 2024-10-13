cf8a1f51177635e9bcf227a44d6ea66e65fba557
samuenoc <sreyesz@unah.hn>
2024-10-12 19:39:27 -0600

    componente de la tarjeta de producto hecha

--- src/app/landing/dashboard/page.tsx
+++ src/app/landing/dashboard/page.tsx
@@ -3,6 +3,7 @@ import useInactivityRedirect from "hooks/useInactivityRedirect";
 import Footer from "../../../components/Footer";
 import Slider from "../components/Slider";
 import Navbar from "components/navbar";
+import Product from "../components/Product";
 
 
 export default function Dashboard(){
@@ -16,6 +17,9 @@ export default function Dashboard(){
     <main className="bg-slate-50 flex-grow w-full">
        <div className="flex align-middle">
        <Slider/>
+       </div>
+       <div className="flex justify-center">
+        <Product nombre="Spiderman Gorro" precio="200L" imagen="/img/imagen34.svg"/>
        </div>
     </main>
     <Footer />
