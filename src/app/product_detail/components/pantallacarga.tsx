a021f4b9953e1e6ade5a0e684cc2f36fc63cfa6d
samuenoc <sreyesz@unah.hn>
2024-10-23 09:28:54 -0600

    pantalla de carga al 100 con animacion

--- src/app/product_detail/components/pantallacarga.tsx
+++ src/app/product_detail/components/pantallacarga.tsx
@@ -1,6 +1,7 @@
 
 import Footer from "components/Footer";
 import Navbar from "components/navbar";
+import LoadingSpinnerSob from "./LoadingSpinnerSob";
 
 
 export default function PantallaCarga() {
@@ -8,6 +9,7 @@ export default function PantallaCarga() {
       <div className="w-full min-h-screen flex flex-col bg-slate-50">
         <Navbar />
         <main className="bg-slate-50 flex-grow w-full flex justify-center">
+          <LoadingSpinnerSob/>
         </main>
         <Footer />
       </div>
