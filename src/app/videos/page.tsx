25cc6dd3dc4c00d915f26c7521cd076d06f514d9
JosueSalomon <josueisacsalomonlanda@gmail.com>
2024-10-24 16:32:49 -0600

    Cambios de imagen de josue, si explota fue josue

--- src/app/videos/page.tsx
+++ src/app/videos/page.tsx
@@ -2,7 +2,6 @@
 import VideoPage from './components/video'; 
 import Navbar from 'components/navbar';
 import Footer from 'components/Footer';
-import QuantitySelector from 'app/product_detail/components/contador';
 
 export default function Dashboard() {
   return (
@@ -11,9 +10,6 @@ export default function Dashboard() {
       <main className="bg-slate-50 flex-grow w-full mt-10">
         <div className="flex  justify-center items-center mt-[40px]">
         <VideoPage/>
-        </div>
-        <div>
-        <QuantitySelector/>
         </div>
       </main>
 
