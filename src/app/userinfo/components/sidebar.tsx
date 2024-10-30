002b3ab7b7d1bc6e61ab66bbf2537a6d00cb1df7
Erick Daniel Escoto Perez <erickdanielescoto@gmail.com>
2024-10-30 02:20:18 -0600

    Componente Perfil

--- src/app/userinfo/components/sidebar.tsx
+++ src/app/userinfo/components/sidebar.tsx
@@ -11,10 +11,10 @@ export default function Sidebar() {
                 />
             </div>
             <div className="w-11/12 h-10 flex items-center justify-center "><h1 className="font-koulen text-slate-700 text-xl rounded-lg ">Nombre apellido</h1></div>
-            <div className="w-8/12 h-10 flex items-center mt-[13%]"><h1 className="font-koulen text-slate-700 text-xl rounded-lg hover:bg-[#C68EFE] hover:text-2xl hover:w-full transition-all duration-300 ease-in-out px-2 hover:text-stone-50 ">Datos Personales</h1></div>
-            <div className="w-8/12 h-10 flex items-center "><h1 className="font-koulen text-slate-700 text-xl rounded-lg hover:bg-[#C68EFE] hover:text-2xl hover:w-full transition-all duration-300 ease-in-out px-2 hover:text-stone-50 ">Historial de Compras</h1></div>
-            <div className="w-8/12 h-10 flex items-center "><h1 className="font-koulen text-slate-700 text-xl rounded-lg hover:bg-[#C68EFE] hover:text-2xl hover:w-full transition-all duration-300 ease-in-out px-2 hover:text-stone-50 ">Mis videos</h1></div>
-            <div className="w-8/12 h-10 flex items-center "><h1 className="font-koulen text-slate-700 text-xl rounded-lg hover:bg-[#C68EFE] hover:text-2xl hover:w-full transition-all duration-300 ease-in-out px-2 hover:text-stone-50 ">Cerrar Sesion</h1></div>
+            <div className="w-[70%] h-10 flex items-center mt-[13%]"><h1 className="font-koulen text-slate-700 text-xl rounded-lg hover:bg-[#C68EFE] hover:text-2xl transition-all duration-300 ease-in-out px-2 hover:text-stone-50">Datos Personales</h1></div>
+            <div className="w-[70%] h-10 flex items-center "><h1 className="font-koulen text-slate-700 text-xl rounded-lg hover:bg-[#C68EFE] hover:text-2xl transition-all duration-300 ease-in-out px-2 hover:text-stone-50 ">Historial de Compras</h1></div>
+            <div className="w-[70%] h-10 flex items-center "><h1 className="font-koulen text-slate-700 text-xl rounded-lg hover:bg-[#C68EFE] hover:text-2xl transition-all duration-300 ease-in-out px-2 hover:text-stone-50 ">Mis videos</h1></div>
+            <div className="w-[70%] h-10 flex items-center "><h1 className="font-koulen text-slate-700 text-xl rounded-lg hover:bg-[#C68EFE] hover:text-2xl transition-all duration-300 ease-in-out px-2 hover:text-stone-50 ">Cerrar Sesion</h1></div>
         </div>
     );
 }
