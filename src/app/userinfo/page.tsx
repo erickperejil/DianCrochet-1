6091eb4920ba1e562cecd82aaf4c50ce425a4a24
samuenoc <sreyesz@unah.hn>
2024-11-02 16:31:50 -0600

    edit perfil

--- src/app/userinfo/page.tsx
+++ src/app/userinfo/page.tsx
@@ -1,13 +1,11 @@
 'use client';
 
 import Navbar from 'components/navbar';
-import Sidebar from './components/sidebar';
 
 export default function Dashboard() {
     return (
         <div className="w-full h-screen flex flex-col bg-slate-50">  
             <Navbar />
-            <Sidebar />
             {/* <Componente Tuyo va aqui /> */}
             <div className="flex-grow">
                 {/* Aquí puedes agregar el contenido principal del Dashboard */}
--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -26,3 +26,16 @@ export interface mail {
 export interface forgotPassword {
   correo: string;
 }
+
+export interface User{
+  nombre: string;
+    apellido: string;
+    correo: string;
+    genero: string;
+    telefono: string;
+    fecha_nacimiento: string;
+    imagen_url: string;
+}
+export interface EditPerfilProps {
+  userEmail: string;
+}
