77d8da29a1d8ffd1467ae152811b43beabdcd64b
ale2002solano <ale134valle@gmail.com>
2024-11-08 08:56:47 -0600

    spinner de carga

--- src/app/profile/fgt-psw-code-edit/page.tsx
+++ src/app/profile/fgt-psw-code-edit/page.tsx
@@ -1,16 +1,21 @@
 "use client";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { Suspense } from "react";
 import PswdCodeFormEdit from "../post/PswdCodeFormEdit";
 
-export default function PswCodeEdit() {
+function EmailParamsComponent() {
   const searchParams = useSearchParams();
   const email = searchParams.get("email");
+  return <PswdCodeFormEdit email={email || ""} />;
+}
 
+export default function PswCodeEdit() {
   return (
     <main className="flex h-screen w-full flex-col bg-slate-50 overflow-hidden">
       <section className="imagen relative flex h-[92%] w-full items-center justify-center mt-16">
-        <PswdCodeFormEdit email={email || ""} />
+        <Suspense fallback={<div>Cargando...</div>}>
+          <EmailParamsComponent />
+        </Suspense>
       </section>
     </main>
   );
