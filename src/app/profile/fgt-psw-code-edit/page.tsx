d1ce8385dd1177f394214da80758fca99693943e
ale2002solano <ale134valle@gmail.com>
2024-11-08 09:04:24 -0600

    spinner de carga

--- src/app/profile/fgt-psw-code-edit/page.tsx
+++ src/app/profile/fgt-psw-code-edit/page.tsx
@@ -1,21 +1,25 @@
 "use client";
 import { useSearchParams } from "next/navigation";
-import React, { Suspense } from "react";
+import React, { useEffect, useState } from "react";
 import PswdCodeFormEdit from "../post/PswdCodeFormEdit";
 
-function EmailParamsComponent() {
-  const searchParams = useSearchParams();
-  const email = searchParams.get("email");
-  return <PswdCodeFormEdit email={email || ""} />;
-}
-
 export default function PswCodeEdit() {
+  const searchParams = useSearchParams();
+  const [email, setEmail] = useState<string | null>(null);
+
+  useEffect(() => {
+    const emailParam = searchParams.get("email");
+    setEmail(emailParam);
+  }, [searchParams]);
+
+  if (email === null) {
+    return <div>Cargando...</div>; // Puedes mostrar un mensaje de carga mientras obtienes el parámetro
+  }
+
   return (
     <main className="flex h-screen w-full flex-col bg-slate-50 overflow-hidden">
       <section className="imagen relative flex h-[92%] w-full items-center justify-center mt-16">
-        <Suspense fallback={<div>Cargando...</div>}>
-          <EmailParamsComponent />
-        </Suspense>
+        <PswdCodeFormEdit email={email || ""} />
       </section>
     </main>
   );
