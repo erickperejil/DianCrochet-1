4e79c86bd5957fa640733a9c077466539200a4e8
ale2002solano <ale134valle@gmail.com>
2024-11-09 08:26:28 -0600

    bug

--- src/app/profile/fgt-psw-code-edit/page.tsx
+++ src/app/profile/fgt-psw-code-edit/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import PswdCodeFormEdit from "../post/PswdCodeFormEdit";
@@ -13,14 +14,16 @@ export default function PswCodeEdit() {
   }, [searchParams]);
 
   if (email === null) {
-    return <div>Cargando...</div>; // Puedes mostrar un mensaje de carga mientras obtienes el parámetro
+    return <div>Cargando...</div>;
   }
 
   return (
-    <main className="flex h-screen w-full flex-col bg-slate-50 overflow-hidden">
-      <section className="imagen relative flex h-[92%] w-full items-center justify-center mt-16">
-        <PswdCodeFormEdit email={email || ""} />
-      </section>
-    </main>
+    <Suspense fallback={<div>Cargando...</div>}>
+      <main className="flex h-screen w-full flex-col bg-slate-50 overflow-hidden">
+        <section className="imagen relative flex h-[92%] w-full items-center justify-center mt-16">
+          <PswdCodeFormEdit email={email || ""} />
+        </section>
+      </main>
+    </Suspense>
   );
 }
