64c6214c57ed7dcd8470e349c297812ba9e2b5c2
ale2002solano <ale134valle@gmail.com>
2024-11-09 08:42:24 -0600

    bug

--- src/app/profile/fgt-psw-code-edit/page.tsx
+++ src/app/profile/fgt-psw-code-edit/page.tsx
@@ -1,29 +1,26 @@
 "use client";
-import { Suspense } from "react";
-import { useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import PswdCodeFormEdit from "../post/PswdCodeFormEdit";
 
 export default function PswCodeEdit() {
-  const searchParams = useSearchParams();
   const [email, setEmail] = useState<string | null>(null);
 
   useEffect(() => {
-    const emailParam = searchParams.get("email");
+    // Usamos window.location para obtener los parámetros de búsqueda
+    const params = new URLSearchParams(window.location.search);
+    const emailParam = params.get("email");
     setEmail(emailParam);
-  }, [searchParams]);
+  }, []);
 
   if (email === null) {
     return <div>Cargando...</div>;
   }
 
   return (
-    <Suspense fallback={<div>Cargando...</div>}>
-      <main className="flex h-screen w-full flex-col bg-slate-50 overflow-hidden">
-        <section className="imagen relative flex h-[92%] w-full items-center justify-center mt-16">
-          <PswdCodeFormEdit email={email || ""} />
-        </section>
-      </main>
-    </Suspense>
+    <main className="flex h-screen w-full flex-col bg-slate-50 overflow-hidden">
+      <section className="imagen relative flex h-[92%] w-full items-center justify-center mt-16">
+        <PswdCodeFormEdit email={email || ""} />
+      </section>
+    </main>
   );
 }
