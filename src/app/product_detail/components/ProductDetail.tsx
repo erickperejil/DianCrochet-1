1b7f71271eb0a37a884832051bd67b0d07d0e9ef
samuenoc <sreyesz@unah.hn>
2024-11-25 23:10:01 -0600

    actualizar los precios

--- src/app/product_detail/components/ProductDetail.tsx
+++ src/app/product_detail/components/ProductDetail.tsx
@@ -1,7 +1,7 @@
 import Image from "next/legacy/image";
-import { useState, useEffect } from 'react';
-import { ProductoDetalle } from '@interfaces/product';
-import { agregarAlCarrito } from '../post/agregarAlCarrito'; // Importa la función del POST
+import { useState, useEffect } from "react";
+import { ProductoDetalle } from "@interfaces/product";
+import { agregarAlCarrito } from "../post/agregarAlCarrito"; // Importa la función del POST
 
 interface ProductDetailProps {
   producto: ProductoDetalle;
@@ -12,58 +12,76 @@ const ProductDetail = ({ producto }: ProductDetailProps) => {
   const [selectedGrosores, setSelectedGrosores] = useState<string | null>(null);
   const [zoomImage, setZoomImage] = useState<string | null>(null);
   const [mensajeExito, setMensajeExito] = useState<string | null>(null);
-  const [mensajeError, setMensajeError] = useState<string | null>(null); // Estado para mensaje de error
-  const [correo, setCorreo] = useState<string>(''); // Estado para almacenar el correo
+  const [mensajeError, setMensajeError] = useState<string | null>(null);
+  const [correo, setCorreo] = useState<string>(""); // Correo del usuario
   const [cantidad, setCantidad] = useState<number>(1);
+  const [precioActual, setPrecioActual] = useState<number>(producto.precio_venta);
 
+  // Función para incrementar la cantidad
   const increaseQuantity = () => {
     setCantidad((prev) => prev + 1);
   };
 
+  // Función para disminuir la cantidad
   const decreaseQuantity = () => {
     if (cantidad > 1) {
       setCantidad((prev) => prev - 1);
     }
   };
 
+  // Efecto para obtener el correo del usuario desde localStorage
   useEffect(() => {
-    // Obtener el correo desde el local storage
-    const storedData = localStorage.getItem('loginResponse');
+    const storedData = localStorage.getItem("loginResponse");
     if (storedData) {
       const parsedData = JSON.parse(storedData);
       const storedCorreo = parsedData?.query_result?.CORREO;
-      console.log('Correo almacenado:', storedCorreo);
-      setCorreo(storedCorreo || ''); // Asignar correo o una cadena vacía
+      setCorreo(storedCorreo || "");
     }
   }, []);
 
-  // Efecto para establecer la primera talla disponible como valor predeterminado
+  // Efecto para establecer la talla predeterminada
   useEffect(() => {
-    if (producto.tallas && producto.tallas.filter(talla => talla !== null).length > 0) {
-      setSelectedTalla(producto.tallas.filter(talla => talla !== null)[0] as string);
+    if (producto.tallas && producto.tallas.filter((talla) => talla !== null).length > 0) {
+      setSelectedTalla(producto.tallas[0]);
     }
   }, [producto.tallas]);
 
-  // Efecto para establecer el primer grosor disponible como valor predeterminado
+  // Efecto para establecer el grosor predeterminado
   useEffect(() => {
-    if (producto.grosores && producto.grosores.filter(grosor => grosor !== null).length > 0) {
-      setSelectedGrosores(producto.grosores.filter(grosor => grosor !== null)[0] as string);
+    if (producto.grosores && producto.grosores.filter((grosor) => grosor !== null).length > 0) {
+      setSelectedGrosores(producto.grosores[0]);
     }
   }, [producto.grosores]);
 
+  // Efecto para actualizar el precio dinámico
+  useEffect(() => {
+    if (selectedTalla && producto.tallas && producto.precios_tallas) {
+      const index = producto.tallas.indexOf(selectedTalla);
+      setPrecioActual(producto.precios_tallas[index]);
+    } else if (selectedGrosores && producto.grosores && producto.precios_grosores) {
+      const index = producto.grosores.indexOf(selectedGrosores);
+      setPrecioActual(producto.precios_grosores[index]);
+    } else {
+      setPrecioActual(producto.precio_venta);
+    }
+  }, [selectedTalla, selectedGrosores, producto]);
+  
+
+  // Manejar el clic en miniaturas
   const handleThumbnailClick = (src: string) => {
     setZoomImage(src);
   };
 
+  // Cerrar la vista de zoom
   const handleZoomClose = () => {
     setZoomImage(null);
   };
 
+  // Manejar la acción de agregar al carrito
   const handleAddToCart = async () => {
     if (!correo) {
-      console.error('No se encontró el correo del usuario en localStorage');
-      setMensajeError('Inicia sesion para comprar'); 
-      setTimeout(() => setMensajeError(null), 3000); 
+      setMensajeError("Inicia sesión para comprar");
+      setTimeout(() => setMensajeError(null), 3000);
       return;
     }
   
@@ -74,31 +92,29 @@ const ProductDetail = ({ producto }: ProductDetailProps) => {
       idProducto,
       cantidadCompra: cantidad,
       talla: selectedTalla || null,
-      grosor: selectedGrosores || null
+      grosor: selectedGrosores || null,
     };
   
     try {
-      console.log('data', data);
       const result = await agregarAlCarrito(data);
-      console.log('Resultado del POST:', result);
   
       if (result.carrito && result.carrito.codigo === 4) {
-        // Mostrar mensaje de error específico de inventario
         setMensajeError(result.carrito.mensaje);
         setTimeout(() => setMensajeError(null), 3000);
       } else {
-        // Mostrar mensaje de éxito
-        setMensajeExito('¡Producto agregado al carrito con éxito!');
+        setMensajeExito("¡Producto agregado al carrito con éxito!");
         setTimeout(() => setMensajeExito(null), 3000);
       }
     } catch (error) {
-      console.error('Error al agregar al carrito:', error);
-      setMensajeError('Hubo un problema con la solicitud'); 
-      setTimeout(() => setMensajeError(null), 3000); 
+      console.error("Error al agregar al carrito:", error); // Muestra detalles en la consola
+      setMensajeError("Hubo un problema con la solicitud");
+      setTimeout(() => setMensajeError(null), 3000);
     }
   };
+  
+
   return (
-    <div className="relative w-max grid grid-cols-1 md:grid-cols-2 gap-[15%] p-8 ">
+    <div className="relative w-max grid grid-cols-1 md:grid-cols-2 gap-[15%] p-8">
       {zoomImage && (
         <div
           className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
@@ -116,17 +132,15 @@ const ProductDetail = ({ producto }: ProductDetailProps) => {
 
       {mensajeExito && (
         <div className="text-lg items-center w-1/4 flex justify-center font-lekton fixed bottom-5 right-5 bg-gray-200 opacity-70 text-purple-900 px-4 py-2 rounded-lg z-50">
-            {mensajeExito}
-           </div>
+          {mensajeExito}
+        </div>
       )}
 
-{mensajeError && (
-             <div className="text-lg items-center w-1/4 flex justify-center font-lekton fixed bottom-5 right-5 bg-gray-200 opacity-70 text-purple-900 px-4 py-2 rounded-lg z-50">
-            {mensajeError}
-           </div>
-
-)}
-
+      {mensajeError && (
+        <div className="text-lg items-center w-1/4 flex justify-center font-lekton fixed bottom-5 right-5 bg-gray-200 opacity-70 text-purple-900 px-4 py-2 rounded-lg z-50">
+          {mensajeError}
+        </div>
+      )}
 
       <div className="flex flex-col space-y-4">
         <div className="flex justify-center">
@@ -166,31 +180,31 @@ const ProductDetail = ({ producto }: ProductDetailProps) => {
 
       <div className="flex flex-col space-y-4">
         <h1 className="text-5xl font-koulen">{producto.nombre_prod}</h1>
-        <p className="text-[#727171] font-robotoMono">L{producto.precio_venta.toFixed(2)}</p>
+        <p className="text-[#727171] font-robotoMono">L{precioActual.toFixed(2)}</p>
         <p className="font-roboto text-[#727171]">*Precio no incluye envío</p>
 
-        {producto.tallas && producto.tallas.filter(talla => talla !== null).length > 0 ? (
+        {producto.tallas && producto.tallas.filter((talla) => talla !== null).length > 0 && (
           <div>
             <h3 className="font-koulen text-[#424242]">TALLA</h3>
             <div className="flex space-x-2 mt-2 font-koulen text-[#424242]">
-              {producto.tallas.filter(talla => talla !== null).map((talla) => (
-                <button
-                  key={talla}
-                  className={`px-11 py-2 border rounded-lg ${
-                    selectedTalla === talla ? 'bg-[#C68EFE] text-white' : 'bg-[#D9D9D9]'
-                  }`}
-                  onClick={() => setSelectedTalla(talla)}
-                >
-                  {talla}
-                </button>
-              ))}
+              {producto.tallas
+                .filter((talla) => talla !== null)
+                .map((talla) => (
+                  <button
+                    key={talla}
+                    className={`px-11 py-2 border rounded-lg ${
+                      selectedTalla === talla ? "bg-[#C68EFE] text-white" : "bg-[#D9D9D9]"
+                    }`}
+                    onClick={() => setSelectedTalla(talla)}
+                  >
+                    {talla}
+                  </button>
+                ))}
             </div>
           </div>
-        ) : (
-          <div></div>
         )}
 
-        {producto.grosores && producto.grosores.filter(grosor => grosor !== null).length > 0 ? (
+        {producto.grosores && producto.grosores.filter((grosor) => grosor !== null).length > 0 && (
           <div>
             <h3 className="font-koulen text-[#424242]">GROSOR</h3>
             <div className="mt-2 font-koulen text-[#D9D9D9] focus:outline-none focus-visible:outline-none focus:ring-0 focus:border-none">
@@ -200,46 +214,44 @@ const ProductDetail = ({ producto }: ProductDetailProps) => {
                 value={selectedGrosores || ""}
                 onChange={(e) => setSelectedGrosores(e.target.value)}
               >
-                <option value="" disabled>Seleccionar</option>
-                {producto.grosores.filter(grosor => grosor !== null).map((grosor) => (
-                  <option key={grosor} value={grosor}>
-                    {grosor}
-                  </option>
-                ))}
+                <option value="" disabled>
+                  Seleccionar
+                </option>
+                {producto.grosores
+                  .filter((grosor) => grosor !== null)
+                  .map((grosor) => (
+                    <option key={grosor} value={grosor}>
+                      {grosor}
+                    </option>
+                  ))}
               </select>
             </div>
           </div>
-        ) : (
-          <div></div>
         )}
 
         <div>
-          <h3 className="font-robotoMono text-[#727171]">Cantidad</h3>
-        </div>
-        <div className="flex flex-col items-start">
-          <div className="flex items-center bg-gray-100 rounded-full shadow-md px-2 py-1">
+          <div className="flex items-center space-x-4">
             <button
+              className="px-4 py-2 bg-[#C68EFE] text-white rounded-md"
               onClick={decreaseQuantity}
-              className="text-xl font-semibold text-[#727171] hover:text-[#C68EFE]"
             >
               -
             </button>
-            <span className="mx-6 text-lg font-medium">{cantidad}</span>
+            <span className="font-bold text-xl">{cantidad}</span>
             <button
+              className="px-4 py-2 bg-[#C68EFE] text-white rounded-md"
               onClick={increaseQuantity}
-              className="text-xl font-semibold text-[#727171] hover:text-[#C68EFE]"
             >
               +
             </button>
           </div>
+          <button
+            className="mt-4 px-6 py-2 bg-[#C68EFE] text-white rounded-md"
+            onClick={handleAddToCart}
+          >
+            Agregar al carrito
+          </button>
         </div>
-
-        <button
-          onClick={handleAddToCart}
-          className="px-4 py-2 mt-4 bg-[#C68EFE] text-white font-semibold rounded-lg shadow-md hover:bg-[#b053fe] transition duration-300"
-        >
-          Agregar al Carrito
-        </button>
       </div>
     </div>
   );
